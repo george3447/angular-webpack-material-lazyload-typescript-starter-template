01376870b8bb18cc146d2363a9da752e3f2c2ca8
Georgekutty Antony <georgekuttyantony@outlook.com>
2018-05-06 16:25:41 +0530

    refactor(app): remove unused codes and dependencies

--- assets/js/lazy-child.62a8ff46eb791aa0c937.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{477:function(n,o){n.exports=""},479:function(n,o,t){},503:function(n,o,t){"use strict";t.r(o);var e=t(7),c=(t(479),{controller:function(){return function(){}}(),template:t(477)}),i=Object(e.module)("lazy-child",[]).component("lazyChildComponent",c).name;o.default=i}}]);
-//# sourceMappingURL=lazy-child.62a8ff46eb791aa0c937.js.map
